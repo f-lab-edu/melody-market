996fd4f1224bfdcf82f638ef2ae88de8c200aafc
elephant97 <elephant0908@gmail.com>
2024-01-09 09:33:38 +0900

    ###프로젝트 초기 세팅
    * bild gradle 세팅
      * spring web
      * 메인 db로 사용할 Mysql connector 추가
      * lombok 추가
      * 테스트 시 사용할 h2 database 추가
      * test code에서도 lombok 사용할 수 있도록 추가
    
    * application.yaml 세팅
      * 8080포트 다른 곳에서 사용중이라 9090포트로 변경

---
+++ src/main/java/com/melodymarket/MelodyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.melodymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melodymarket/MelodyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melodymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

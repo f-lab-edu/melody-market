b2a459bcb185b0748e0c740c3a4910292ef89bab
elephant97 <elephant0908@gmail.com>
2024-01-16 14:40:53 +0900

    [#4] 포맷팅 정렬 * 단축키 활용하여 포맷팅 정렬 * sonalint의 가이드 대로 메서드 네이밍 규칙 수정

--- src/main/java/com/melodymarket/admin/controller/HelloController.java
+++ src/main/java/com/melodymarket/admin/controller/HelloController.java
@@ -1,14 +1,12 @@
 package com.melodymarket.admin.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
-    @GetMapping ("/hello")
-    @ResponseBody
-    public String HelloReturn() {
+    @GetMapping("/hello")
+    public String helloReturn() {
         return "hello world";
     }
 

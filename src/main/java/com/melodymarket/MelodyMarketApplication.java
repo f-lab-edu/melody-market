cbc18ade301c8abad9a181e352e554c33a082910
elephant97 <elephant0908@gmail.com>
2024-01-16 12:28:42 +0900

    #3 - 데이터베이스(Mysql, Mybatis)연동
    * Mysql 연결
      * url, username, password는 민감 정보이므로 분리하여 커밋되지 않도록 함
    * Mybatis 연결
      * Mybatis dependency 추가
      * mapper 위치 지정

--- src/main/java/com/melodymarket/MelodyMarketApplication.java
+++ src/main/java/com/melodymarket/MelodyMarketApplication.java
@@ -2,8 +2,10 @@ package com.melodymarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/keys.properties")
 public class MelodyMarketApplication {
 
 	public static void main(String[] args) {

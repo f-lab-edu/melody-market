8c27ce2a9b8136baf4c49ce93477c42b898189bc
elephant97 <elephant0908@gmail.com>
2024-02-18 02:11:30 +0900

    [#37] 공연장 정보 등록 API 구현 - 예외 및 yaml 설정 추가
    * jpa 실행 시 sql 확인하기 위한 옵션 추가
    * 데이터베이스 접근 오류 발생 시 예외 핸들러 추가

--- src/main/java/com/melodymarket/infrastructure/exception/DatabaseExceptionHandler.java
+++ src/main/java/com/melodymarket/infrastructure/exception/DatabaseExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.melodymarket.infrastructure.exception;
 
+import com.melodymarket.common.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,5 +22,12 @@ public class DatabaseExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(ex.getMessage());
 
+    }
+
+    @ExceptionHandler(DataAccessCustomException.class)
+    public ResponseDto<Object> handleDataAccessException(DataAccessCustomException ex) {
+        return ResponseDto.of(HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                "");
     }
 }

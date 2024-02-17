175fe3d90d1ee2183c24202338e3d508318b9752
elephant97 <elephant0908@gmail.com>
2024-02-18 02:08:03 +0900

    [#37] 공연장 정보 등록 API 구현 - 테스트 추가
    * 공연장 등록 Controller, Service 추가
    * 테스트 완료 확인

---
+++ src/test/java/com/melodymarket/presentation/theater/controller/ManageTheaterControllerTest.java
@@ -0,0 +1,99 @@
+package com.melodymarket.presentation.theater.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.melodymarket.application.theater.dto.TheaterDto;
+import com.melodymarket.application.theater.dto.TheaterRoomDto;
+import com.melodymarket.application.theater.dto.TheaterSeatDto;
+import com.melodymarket.application.theater.service.ManageTheaterServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("공연장 API 테스트")
+@WithMockUser(roles = "USER")
+@WebMvcTest(ManageTheaterController.class)
+class ManageTheaterControllerTest {
+
+    @MockBean
+    ManageTheaterServiceImpl manageTheaterService;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ManageTheaterController manageTheaterController;
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .defaultRequest(post("/**").with(csrf()))
+                .build();
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    @DisplayName("[POST] 공연장 등록 API 테스트")
+    void givenTestTheater_whenAddTheater_thenSuccess() throws Exception {
+        //given
+        TheaterDto testTheater = createTestTheater();
+        //json 형식으로 convert
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(testTheater);
+
+        //when
+        mockMvc.perform(post("/v1/theater/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                //then
+                .andExpect(status().isOk());
+    }
+
+    private TheaterDto createTestTheater() {
+        return TheaterDto.builder()
+                .name("공연장")
+                .location("위치")
+                .roomsInfo(createTestTheaterRooms())
+                .build();
+    }
+
+    private List<TheaterRoomDto> createTestTheaterRooms() {
+        List<TheaterRoomDto> roomDtoList = new ArrayList<>();
+        roomDtoList.add(
+                TheaterRoomDto.builder()
+                        .roomName("test hall")
+                        .seatData(createTestTheaterSeats())
+                        .build()
+        );
+        return roomDtoList;
+    }
+
+    private List<TheaterSeatDto> createTestTheaterSeats() {
+        List<TheaterSeatDto> seatDtoList = new ArrayList<>();
+        seatDtoList.add(
+                TheaterSeatDto.builder()
+                        .seatFloor(1)
+                        .seatRow(1)
+                        .seatNumber(1)
+                        .build()
+        );
+        return seatDtoList;
+    }
+}

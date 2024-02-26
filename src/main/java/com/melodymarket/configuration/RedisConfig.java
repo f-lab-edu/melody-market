56f710304c957e8c3c1727f18b6d145e48fe0a26
elephant97 <elephant0908@gmail.com>
2024-02-26 23:03:15 +0900

    [#36] CICD 구성 - redis
    * redis host 127.0.0.1로 변경

--- src/main/java/com/melodymarket/configuration/RedisConfig.java
+++ src/main/java/com/melodymarket/configuration/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.melodymarket.configuration;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +14,6 @@ public class RedisConfig {
     private String host;
     @Value("${spring.data.redis.port}")
     private int port;
-
-    @PostConstruct
-    public void postConstruct() {
-        System.out.println("Redis host: " + host);
-        System.out.println("Redis port: " + port);
-    }
 
 
     @Bean
